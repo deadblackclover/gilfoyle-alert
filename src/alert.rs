12805007a9a14966fd16bd899744eb8e9fa150ec
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-07-28 11:53:53 +0700

    Replace unwraps

--- src/alert.rs
+++ src/alert.rs
@@ -1,5 +1,6 @@
 use play;
 use reqwest::blocking::Client;
+use reqwest::Error;
 use serde_json::Value;
 use std::io::Read;
 
@@ -9,25 +10,30 @@ const API: &str =
 
 /// Function for check price
 pub fn check(coin: String, price_str: String) {
-    let body = get(String::from(API));
-    let json: Value = serde_json::from_str(&body).unwrap();
+    let body = get(String::from(API)).expect("Failed to get usd price");
+    let json: Value = serde_json::from_str(&body).expect("Failed to parse json");
 
     let usd_str = &json["datas"]["usd"][&coin];
-    let usd = usd_str.as_str().unwrap().parse::<f32>().unwrap();
+    let usd = usd_str
+        .as_str()
+        .unwrap()
+        .parse::<f32>()
+        .expect("Failed to parse string");
 
-    let price = price_str.parse::<f32>().unwrap();
+    let price = price_str.parse::<f32>().expect("Failed to parse string");
 
     if usd < price {
-        println!("ALERT! PRICE: {}", usd);
-        play::play("baaah.mp3").unwrap();
+        println!("ALERT! PRICE {}: {}", coin.to_uppercase(), usd);
+        play::play("baaah.mp3").expect("Failed to play mp3");
     }
 }
 
 /// Function for get requests
-fn get(url: String) -> String {
+fn get(url: String) -> Result<String, Error> {
     let client = Client::new();
-    let mut res = client.get(&url).send().unwrap();
+    let mut res = client.get(&url).send()?;
     let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
-    body
+    res.read_to_string(&mut body)
+        .expect("Failed to read string");
+    Ok(body)
 }
